e4f612d57283bb537e610b45281d072e4c0302cd
Sarah Chen <sarahlc888@gmail.com>
2020-10-01 15:59:59 -0400

    remove original main

--- main/main.py
+++
@@ -1,9 +0,0 @@
-short_words = ["the", "in", "a", "as", "to", "be", "an", "for", "nor", "but", "yet", "so", "at", "around", "by", "after", "along", "for", "from", "of", "on", "to", "with", "without"]
-
-def titlecaps(s):
-    words = s.lower().split()
-    for i in range(0, len(words)):
-        if words[i] not in short_words:
-            words[i] = words[i].capitalize()
-    words[0] = words[0].capitalize()
-    return " ".join(words)
